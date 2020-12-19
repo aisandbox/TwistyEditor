0c086f398feb95ff27a7c2cfbd3ba34faac6b0e5
Graham <graham-evans@users.noreply.github.com>
2020-12-19 15:59:45 +0000

    xstream tool

--- src/main/java/dev/aisandbox/twistyeditor/PuzzleUtil.java
+++ src/main/java/dev/aisandbox/twistyeditor/PuzzleUtil.java
@@ -1,26 +1,38 @@
 package dev.aisandbox.twistyeditor;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import dev.aisandbox.twistyeditor.model.Cell;
+import dev.aisandbox.twistyeditor.model.CompiledMove;
+import dev.aisandbox.twistyeditor.model.Loop;
+import dev.aisandbox.twistyeditor.model.Move;
 import dev.aisandbox.twistyeditor.model.Puzzle;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PuzzleUtil {
 
-  public static void savePuzzleToFile(Puzzle puzzle, File file) throws IOException {
-    FileOutputStream fileOutputStream
-        = new FileOutputStream(file);
-    ObjectOutputStream objectOutputStream
-        = new ObjectOutputStream(fileOutputStream);
-    objectOutputStream.writeObject(puzzle);
-    objectOutputStream.flush();
-    objectOutputStream.close();
+  public static XStream getCodec() {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(Puzzle.class);
+    xstream.registerConverter(new BufferedImageConverter());
+    xstream.addPermission(NoTypePermission.NONE);
+    xstream.allowTypes(
+        new Class[] {
+          Puzzle.class,
+          Cell.class,
+          Move.class,
+          Loop.class,
+          CompiledMove.class,
+          HashMap.class,
+          Map.class,
+          List.class,
+          ArrayList.class,
+          String.class,
+            Integer.class
+        });
+    return xstream;
   }
-
-  public static void loadPuzzleFromFile(Puzzle puzzle,File file) {
-    // load
-  }
-
 }
